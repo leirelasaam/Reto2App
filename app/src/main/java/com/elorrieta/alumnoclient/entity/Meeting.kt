202a1cc4836ab7ed6fbea05614839040efd742db
DESKTOP-SNU5A6J\Violeta <violeta.uriengr@elorrieta-errekamari.com>
2025-01-19 21:20:09 +0100

    Creando reuniones

--- app/src/main/java/com/elorrieta/alumnoclient/entity/Meeting.kt
+++ app/src/main/java/com/elorrieta/alumnoclient/entity/Meeting.kt
@@ -1,4 +1,16 @@
 package com.elorrieta.alumnoclient.entity
+import java.time.LocalDateTime
 
-enum class Meeting {
+enum class Status {
+    ACEPTADA, RECHAZADA, PENDIENTE
 }
+data class Meeting(
+    val id: Long, // BIGINT
+    val userId: Long,
+    val day: Int, // TINYINT
+    val time: Int,
+    val week: Int, // TINYINT
+    val status: Status, // ENUM
+    val createdAt: LocalDateTime, // TIMESTAMP
+    val updatedAt: LocalDateTime
+)
