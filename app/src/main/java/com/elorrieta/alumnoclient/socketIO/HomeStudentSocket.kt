4ba28047bb5ee5511d277704c5f7883c8796ccb3
Lucian <lucianstelian.grecianu@elorrieta-errekamari.com>
2025-02-02 23:44:47 +0100

    Arreglando bugs.

--- app/src/main/java/com/elorrieta/alumnoclient/socketIO/HomeStudentSocket.kt
+++ app/src/main/java/com/elorrieta/alumnoclient/socketIO/HomeStudentSocket.kt
@@ -18,7 +18,7 @@ import org.json.JSONObject
 
 class HomeStudentSocket(private val activity: Activity) {
     // Server IP:Port
-    private val ipPort = "http://10.5.104.38:3000"
+    private val ipPort = "http://192.168.1.135:3000"
     private val socket: Socket = IO.socket(ipPort)
 
     // For log purposes
