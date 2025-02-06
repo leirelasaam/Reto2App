665aac7ae82af38dc8e180b8a68bbb1bba845528
dayanaandrea <dayanaandrea.fernandezla@elorrieta-errekamari.com>
2025-02-06 19:55:08 +0100

    Se crea la conexión en todas las ventanas

--- app/src/main/java/com/elorrieta/alumnoclient/socketIO/HomeStudentSocket.kt
+++ app/src/main/java/com/elorrieta/alumnoclient/socketIO/HomeStudentSocket.kt
@@ -1,17 +1,13 @@
 package com.elorrieta.alumnoclient.socketIO
 
-import android.app.Activity
-import android.graphics.Typeface
 import android.util.Log
-import android.view.Gravity
-import android.widget.LinearLayout
-import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.gridlayout.widget.GridLayout
 import com.elorrieta.alumnoclient.R
+import com.elorrieta.alumnoclient.StudentScheduleActivity
 import com.elorrieta.alumnoclient.entity.StudentSchedule
 import com.elorrieta.alumnoclient.singletons.LoggedUser
+import com.elorrieta.alumnoclient.singletons.PrivateKeyManager
 import com.elorrieta.alumnoclient.singletons.SocketConnectionManager
 import com.elorrieta.alumnoclient.socketIO.config.Events
 import com.elorrieta.alumnoclient.socketIO.model.MessageInput
@@ -21,9 +17,9 @@ import com.elorrieta.alumnoclient.utils.JSONUtil
 import com.elorrieta.alumnoclient.utils.Util
 import org.json.JSONObject
 
-class HomeStudentSocket(private val activity: Activity) {
+class HomeStudentSocket(private val activity: StudentScheduleActivity) {
     private var tag = "socket.io"
-    private var key = AESUtil.loadKey(activity)
+    private var key = PrivateKeyManager.getKey(activity)
     private val socket = SocketConnectionManager.getSocket()
 
     init {
@@ -36,7 +32,7 @@ class HomeStudentSocket(private val activity: Activity) {
                 val gridLayout = activity.findViewById<GridLayout>(R.id.gridLayoutStudent)
 
                 activity.runOnUiThread {
-                    loadScheduleSkeleton(gridLayout)
+                    activity.loadScheduleSkeleton(gridLayout)
                 }
 
                 if (mi.code == 200) {
@@ -55,54 +51,21 @@ class HomeStudentSocket(private val activity: Activity) {
 
                     // Actualización de la UI en el hilo principal
                     activity.runOnUiThread {
-                        schedules.forEach { (module, day, hour) ->
+                        activity.loadStudentSchedule(schedules, gridLayout)
 
-                            val container = LinearLayout(activity)
-                            container.orientation = LinearLayout.HORIZONTAL
-                            container.gravity = Gravity.CENTER
-
-                            val textView = TextView(activity)
-                            textView.text = module
-                            textView.gravity = Gravity.CENTER
-                            textView.setTypeface(null, Typeface.BOLD)
-                            textView.setBackgroundColor(activity.getColor(R.color.pink))
-                            textView.setTextColor(
-                                ContextCompat.getColor(
-                                    activity,
-                                    R.color.white
-
-                                )
-                            )
-
-                            val layoutParams = LinearLayout.LayoutParams(
-                                LinearLayout.LayoutParams.MATCH_PARENT,
-                                LinearLayout.LayoutParams.WRAP_CONTENT
-                            )
-                            textView.layoutParams = layoutParams
-
-                            container.addView(textView)
-
-                            val params = GridLayout.LayoutParams()
-                            // Los eventos serán distribuidos en las filas y columnas según su hora y día
-                            params.rowSpec = GridLayout.spec(hour, 1f)
-                            params.columnSpec = GridLayout.spec(day, 1f)
-                            container.layoutParams = params
-                            gridLayout.addView(container)
-                        }
-
-                        Toast.makeText(
+                        /*Toast.makeText(
                             activity,
-                            "Horario cargado",
+                            activity.getString(R.string.schedules_200),
                             Toast.LENGTH_SHORT
-                        ).show()
+                        ).show()*/
 
                     }
                 } else {
                     var error = ""
                     when (mi.code) {
-                        400 -> error = "Semana no lectiva"
-                        404 -> error = "No hay horario cargado para esa semana"
-                        500 -> error = "No se ha podido cargar el horario"
+                        400 -> error = activity.getString(R.string.schedules_400)
+                        404 -> error = activity.getString(R.string.schedules_404)
+                        500 -> error = activity.getString(R.string.schedules_500)
                     }
                     activity.runOnUiThread {
                         Toast.makeText(
@@ -125,63 +88,5 @@ class HomeStudentSocket(private val activity: Activity) {
         socket.emit(Events.ON_STUDENT_SCHEDULE.value, encryptedMsg)
 
         Log.d(tag, "Attempt of get schedules - $message")
-    }
-
-
-    private fun loadScheduleSkeleton(gridLayout: GridLayout) {
-        activity.runOnUiThread {
-            // FALTA PASAR ESTO A STRINGS
-            val dias = listOf("LUNES", "MARTES", "MIÉRCOLES", "JUEVES", "VIERNES")
-            val horas = listOf("15:00", "16:00", "17:00", "18:00", "19:00", "20:00")
-
-            // vaciar primero
-            gridLayout.removeAllViews()
-
-            // Añadir una columna vacía, es para dar color al bg
-            val txt = TextView(activity)
-            txt.setBackgroundColor(ContextCompat.getColor(activity, R.color.pantone_dark))
-            txt.setTextColor(ContextCompat.getColor(activity, R.color.white))
-            val param = GridLayout.LayoutParams()
-            param.rowSpec = GridLayout.spec(0, 0.5f)
-            param.columnSpec = GridLayout.spec(0, 1f)
-            txt.layoutParams = param
-            gridLayout.addView(txt)
-
-            // Añadir los días en la primera fila
-            for (i in dias.indices) {
-                val textView = TextView(activity)
-                textView.text = dias[i]
-                textView.gravity = Gravity.CENTER
-                textView.setTypeface(null, Typeface.BOLD)
-
-                textView.setBackgroundColor(
-                    ContextCompat.getColor(
-                        activity,
-                        R.color.pantone_dark
-                    )
-                )
-                textView.setTextColor(ContextCompat.getColor(activity, R.color.white))
-
-                val params = GridLayout.LayoutParams()
-                params.rowSpec = GridLayout.spec(0, 0.5f)
-                params.columnSpec = GridLayout.spec(i + 1, 1f)
-                textView.layoutParams = params
-                gridLayout.addView(textView)
-            }
-
-            // Añadir las horas en la primera columna
-            for (i in horas.indices) {
-                val textView = TextView(activity)
-                textView.text = horas[i]
-                textView.gravity = Gravity.CENTER
-                textView.setTypeface(null, Typeface.BOLD)
-
-                val params = GridLayout.LayoutParams()
-                params.rowSpec = GridLayout.spec(i + 1, 1f)
-                params.columnSpec = GridLayout.spec(0, 1f)
-                textView.layoutParams = params
-                gridLayout.addView(textView)
-            }
-        }
     }
 }
